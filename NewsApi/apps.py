68bb5451885bda5ea2a7ab69bd49738d46b331aa
rudrakshi99 <sonirudrakshi99@gmail.com>
2021-05-17 22:07:38 +0530

    basic api setup

---
+++ NewsApi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NewsapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'NewsApi'
--- NewsPostApi/settings.py
+++ NewsPostApi/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'NewsApi',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
