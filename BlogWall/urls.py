1e95d88f51dd3805c241189bdd7d1669ac738021
rudrakshi99 <sonirudrakshi99@gmail.com>
2021-05-24 21:21:55 +0530

    initial setup

--- BlogWall/urls.py
+++ BlogWall/urls.py
@@ -1,4 +1,4 @@
-"""BlogPost URL Configuration
+"""BlogWall URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
--- BlogWall/wsgi.py
+++ BlogWall/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for BlogPost project.
+WSGI config for BlogWall project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BlogPost.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BlogWall.settings')
 
 application = get_wsgi_application()
