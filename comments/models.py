9da7affc92f1e95bcd5ae91b40b266040a048ae0
rudrakshi99 <sonirudrakshi99@gmail.com>
2021-07-05 18:44:12 +0530

    added create function in model

--- comments/models.py
+++ comments/models.py
@@ -17,6 +17,24 @@ class CommentManager(models.Manager):
         obj_id = instance.id
         qs = super(CommentManager, self).filter(content_type=content_type, object_id= obj_id).filter(parent=None)
         return qs
+    
+    def create_by_model_type(self, model_type, slug, content, user, parent_obj=None):
+        model_qs = ContentType.objects.filter(model=model_type)
+        if model_qs.exists():
+            SomeModel = model_qs.first().model_class()
+            obj_qs = SomeModel.objects.filter(slug=slug)
+            if obj_qs.exists() and obj_qs.count() == 1:
+                instance = self.model()
+                instance.content = content
+                instance.user = user
+                instance.content_type = model_qs.first()
+                instance.object_id = obj_qs.first().id
+                if parent_obj:
+                    instance.parent = parent_obj
+                instance.save()
+                return instance
+        return None
+
 
 class Comment(models.Model):
     user        = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
